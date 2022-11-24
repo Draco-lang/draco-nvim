3a2650da70b424d9f2a50ee4d5221b19441faedc
WhiteBlackGoose <wbg@angouri.org>
2022-11-24 19:47:54 +0300

    smh

--- lua/draco-nvim/init.lua
+++ lua/draco-nvim/init.lua
@@ -1,7 +1,31 @@
+-- https://stackoverflow.com/questions/1340230/check-if-directory-exists-in-lua
+function exists(file)
+   local ok, err, code = os.rename(file, file)
+   if not ok then
+      if code == 13 then
+         -- Permission denied, but it exists
+         return true
+      end
+   end
+   return ok
+end
+
+local LS_url = 'https://github.com/Draco-lang/Compiler'
+local LS_path = 'draco-server/src/Draco.LanguageServer/bin/Debug/net7.0'
+
+function DracoPullServer()
+    os.execute('rm draco-server')
+    os.execute('git clone ' .. LS_url .. ' draco-server')
+end
+
+function DracoBuildServer()
+    os.execute('dotnet build --project draco-server/src/Draco.LanguageServer/Draco.LanguageServer.csproj')
+end
+
 DracoRunServer = function()
    vim.lsp.start({
        name = 'draco',
-       cmd={LS_hotfix_path},
+       cmd={LS_path},
        root_dir=vim.fs.dirname('src'),
        detached=false,
        on_attach = function(_, bufnr)
@@ -11,11 +35,9 @@ DracoRunServer = function()
        })
 end
 vim.cmd[[
+:command DracoPullServer lua DracoPullServer()
+:command DracoBuildServer lua DracoBuildServer()
 :command DracoRunServer lua DracoRunServer()
-]]
-vim.cmd[[
 :autocmd FileType draco DracoRunServer
-]]
-vim.cmd[[
 :au BufRead,BufNewFile *.draco set filetype=draco
 ]]
